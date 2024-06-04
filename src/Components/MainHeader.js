d951d166fcf26647661ae86d63a6e8df06755d5d
Ioane <ioane.natroshvili08b@geolab.edu.ge>
2024-06-04 20:30:55 +0400

    asdasd

--- src/Components/MainHeader.js
+++ src/Components/MainHeader.js
@@ -14,12 +14,12 @@ const MainHeader = ({ darkMode, changeDarkMode }) => {
                         w-20 h-9 bg-white dark:bg-slate-300 shadow-[0px_1px_5px_2px_rgba(0,0,0,0.1)] peer-focus:outline-0 peer-focus:ring-transparent rounded-full peer transition-all ease-in-out duration-500
                     `}></div>
                     <span className={`
-              absolute w-8 h-8  top-[2px] left-[2px] peer-checked:left-[14px] rounded-full peer transition-all duration-500 peer-checked:translate-x-full
-              flex items-center justify-center
-              text-amber-500
-              peer-checked:text-slate-800
-              text-2xl
-            `}>{darkMode ? <FaCloudMoon /> : <FaCloudSun />}</span>
+                        absolute w-8 h-8  top-[2px] left-[2px] peer-checked:left-[14px] rounded-full peer transition-all duration-500 peer-checked:translate-x-full
+                        flex items-center justify-center
+                        text-amber-500
+                        peer-checked:text-slate-800
+                        text-2xl
+                    `}>{darkMode ? <FaCloudMoon /> : <FaCloudSun />}</span>
                 </label>
             </div>
         </header>
