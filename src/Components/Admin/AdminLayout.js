eb16caf044c5fe443676dc97c234de582a596c11
Ioane <ioane.natroshvili08b@geolab.edu.ge>
2024-06-08 19:49:43 +0400

    asdasd

--- src/Components/Admin/AdminLayout.js
+++ src/Components/Admin/AdminLayout.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const AdminLayout = () => {
+
     return (
         <div className="py-2 pt-3 flex flex-1 gap-3 alk-sanet">
             <div className="text-gray-600 dark:text-slate-200 flex hidden xl:block xl:basis-1/5 transition-colors duration-700">
                 <div className="p-3 bg-white dark:bg-slate-700 text-center text-lg rounded-md transition-colors duration-700">
-                    <b className="text-gray-600 dark:text-slate-200">ჯგუფები</b>
+                    <b className="text-gray-600 dark:text-slate-200">თემატიკები</b>
                 </div>
                 <div className="mt-3">
+                    <div id="accordion-collapse" data-accordion="collapse" className="rounded-md bg-white dark:bg-slate-700 overflow-hidden transition-colors duration-700">
+                        <h2 id="accordion-collapse-heading-1">
+                            <button type="button" className="flex overflow-hidden outline-none items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-600 dark:text-gray-400 hover:bg-slate-100 dark:hover:bg-slate-600 gap-3 dark:bg-slate-700 transition-colors duration-700" data-accordion-target="#accordion-collapse-body-1" aria-expanded="false" aria-controls="accordion-collapse-body-1">
+                                <span>მათემატიკა</span>
+                                <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5" />
+                                </svg>
+                            </button>
+                        </h2>
+                        <div id="accordion-collapse-body-1" className="hidden" aria-labelledby="accordion-collapse-heading-1">
+                            <div className="p-2 text-gray-600 dark:text-slate-200 dark:border-gray-700 transition-none">
+                                <div className="p-3 border border-1 border-teal-300 dark:border-slate-500 rounded-md">
+                                    <p>მათემატიკური ტესტი 1</p>
+                                    <div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="bg-white dark:bg-slate-700 dark:text-slate-200 flex flex-1 p-2 rounded-md transition-colors duration-700">
--- src/Components/MainHeader.js
+++ src/Components/MainHeader.js
@@ -14,12 +14,12 @@ const MainHeader = ({ darkMode, changeDarkMode }) => {
                         <button data-collapse-toggle="header-navbar" type="button"
                             className="inline-flex items-center ml-auto mr-3 p-2 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                             aria-controls="header-navbar" aria-expanded="false">
-                            <span class="sr-only">Open main menu</span>
+                            <span className="sr-only">Open main menu</span>
                             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
                                 xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
+                                <path fillRule="evenodd"
                                     d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                                    clip-rule="evenodd"></path>
+                                    clipRule="evenodd"></path>
                             </svg>
                         </button>
                         <div className="hidden absolute bg-white dark:bg-slate-700 lg:static left-0 top-[95%] w-full lg:w-auto lg:flex p-2 lg:pl-11 shadow-xl lg:shadow-none rounded-b-lg transition-colors duration-700 ml-auto" id="header-navbar">
