11c30f97202ee5269bf747138680903c0259ae4f
Ioane <ioane.natroshvili08b@geolab.edu.ge>
2024-06-08 17:39:14 +0400

    asdasd

--- src/App.js
+++ src/App.js
@@ -6,10 +6,14 @@ import Login from './Components/Login';
 
 import { initFlowbite } from 'flowbite';
 import AdminLayout from './Components/Admin/AdminLayout';
+import Loading from './Components/Loading';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
   const [moding, setModing] = useState(false);
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadingAnim, setLoadingAnim] = useState(true);
 
 
   const changeDarkMode = e => {
@@ -28,11 +32,19 @@ function App() {
     const _darkMode = localStorage.getItem('darkMode');
     if (_darkMode === "false") setDarkMode(false);
     else setDarkMode(true);
+
+    setTimeout(() => {
+      setLoadingAnim(false);
+    }, 1500);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
   }, []);
 
 
   return (
     <div className={`bg-teal-300 dark:bg-slate-800 flex flex-col h-screen p-3 ${darkMode ? 'dark' : 'light'}`}>
+      {isLoading && <Loading loadingAnim={loadingAnim} />}
       <MainHeader darkMode={darkMode} changeDarkMode={changeDarkMode} />
       {/* <Login /> */}
       <AdminLayout />
