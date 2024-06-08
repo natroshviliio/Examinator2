6fbd62fd3bc9458a46bca9efaff5c163f1119832
Ioane <ioane.natroshvili08b@geolab.edu.ge>
2024-06-08 20:35:55 +0400

    asdasd

--- src/Components/Admin/AdminLayout.js
+++ src/Components/Admin/AdminLayout.js
@@ -1,4 +1,8 @@
 import React, { useEffect } from "react";
+import { MdTimer, MdTask } from "react-icons/md";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { IoIosRefresh } from "react-icons/io";
+import { GrCopy } from "react-icons/gr";
 
 const AdminLayout = () => {
 
@@ -21,9 +25,20 @@ const AdminLayout = () => {
                         <div id="accordion-collapse-body-1" className="hidden" aria-labelledby="accordion-collapse-heading-1">
                             <div className="p-2 text-gray-600 dark:text-slate-200 dark:border-gray-700 transition-none">
                                 <div className="p-3 border border-1 border-teal-300 dark:border-slate-500 rounded-md">
-                                    <p>მათემატიკური ტესტი 1</p>
-                                    <div>
-
+                                    <p className="pb-1 border-b border-teal-300 dark:border-slate-500">მათემატიკური ტესტი 1</p>
+                                    <div className="text-sm mt-3 flex flex-col gap-1">
+                                        <p className="flex items-center"><MdTask className="text-lg mb-1" /> კითხვების რაოდენობა: 50</p>
+                                        <p className="flex items-center"><MdTimer className="text-lg mb-1" /> ჯამური დრო: 01:15:00</p>
+                                        <p className="flex items-center">
+                                            <RiLockPasswordFill className="text-lg mb-1" />
+                                            მოწვევის კოდი: LzJmP2A53T4
+                                            <button className="ms-2 text-sm p-1 rounded-full bg-emerald-400 hover:bg-emerald-500 active:bg-emerald-600 outline-none mb-1 dark:bg-slate-500 dark:hover:bg-slate-600 dark:active:bg-slate-800"><GrCopy /></button>
+                                            <button className="ms-2 text-sm p-1 rounded-full bg-emerald-400 hover:bg-emerald-500 active:bg-emerald-600 outline-none mb-1 dark:bg-slate-500 dark:hover:bg-slate-600 dark:active:bg-slate-800"><IoIosRefresh /></button>
+                                        </p>
+                                    </div>
+                                    <div className="mt-3 rounded-sm border border-teal-300 dark:border-slate-500 p-2 flex gap-2">
+                                        <button className="text-md w-1/2 py-1 px-2 rounded bg-emerald-400 hover:bg-emerald-500 active:bg-emerald-600 outline-none dark:bg-slate-500 dark:hover:bg-slate-600 dark:active:bg-slate-800">გააქტიურება</button>
+                                        <button className="text-md w-1/2 py-1 px-2 rounded bg-emerald-400 hover:bg-emerald-500 active:bg-emerald-600 outline-none dark:bg-slate-500 dark:hover:bg-slate-600 dark:active:bg-slate-800">რედაქტირება</button>
                                     </div>
                                 </div>
                             </div>
