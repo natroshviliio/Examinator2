461aeedca4255a357000bf02dbc9242cd97f54b1
Ioane <ioane.natroshvili08b@geolab.edu.ge>
2024-06-10 17:16:56 +0400

    123123

--- src/Components/Admin/Dashboard.js
+++ src/Components/Admin/Dashboard.js
@@ -8,40 +8,40 @@ import { FaUserGroup } from "react-icons/fa6";
 const Dashboard = () => {
     return (
         <div className="px-3 py-2 flex flex-col md:flex-row items-start items-center md:justify-start gap-3 w-full">
-            <div className="p-4 bg-teal-100/25 dark:bg-slate-500/25 border border-teal-400 dark:border-slate-500 rounded-md w-11/12 md:w-4/12 lg:w-3/12 text-teal-500 dark:text-slate-400 shadow-lg">
+            <div className="p-4 bg-teal-100/25 hover:bg-teal-100 dark:bg-slate-500/25 dark:hover:bg-slate-500/50 border border-teal-400 dark:border-slate-500 rounded-md w-11/12 md:w-4/12 lg:w-3/12 text-teal-500 dark:text-slate-400 shadow-lg transition-colors duration-500">
                 <div className="flex justify-center text-7xl py-3">
                     <IoBookSharp />
                 </div>
                 <div className="text-center">
                     <p className="text-2xl">თემატიკები</p>
-                    <button className="mt-6 py-1 px-5 rounded border border-teal-500 hover:bg-teal-500 hover:text-white dark:border-slate-400 dark:hover:bg-slate-400 transition-colors duration-300">შექმნა</button>
+                    <button className="mt-6 py-1 px-5 rounded border border-teal-500 hover:bg-teal-500 hover:text-white dark:border-slate-400 dark:hover:bg-slate-400 transition-colors duration-300">დამატება</button>
                 </div>
             </div>
-            <div className="p-4 bg-teal-100/25 dark:bg-slate-500/25 border border-teal-400 dark:border-slate-500 rounded-md w-11/12 md:w-4/12 lg:w-3/12 text-teal-500 dark:text-slate-400 shadow-lg">
+            <div className="p-4 bg-teal-100/25 hover:bg-teal-100 dark:bg-slate-500/25 dark:hover:bg-slate-500/50 border border-teal-400 dark:border-slate-500 rounded-md w-11/12 md:w-4/12 lg:w-3/12 text-teal-500 dark:text-slate-400 shadow-lg transition-colors duration-500">
                 <div className="flex justify-center text-7xl py-3">
                     <MdTask />
                 </div>
                 <div className="text-center">
                     <p className="text-2xl">ტესტები</p>
-                    <button className="mt-6 py-1 px-5 rounded border border-teal-500 hover:bg-teal-500 hover:text-white dark:border-slate-400 dark:hover:bg-slate-400 transition-colors duration-300">შექმნა</button>
+                    <button className="mt-6 py-1 px-5 rounded border border-teal-500 hover:bg-teal-500 hover:text-white dark:border-slate-400 dark:hover:bg-slate-400 transition-colors duration-300">დამატება</button>
                 </div>
             </div>
-            <div className="p-4 bg-teal-100/25 dark:bg-slate-500/25 border border-teal-400 dark:border-slate-500 rounded-md w-11/12 md:w-4/12 lg:w-3/12 text-teal-500 dark:text-slate-400 shadow-lg">
+            <div className="p-4 bg-teal-100/25 hover:bg-teal-100 dark:bg-slate-500/25 dark:hover:bg-slate-500/50 border border-teal-400 dark:border-slate-500 rounded-md w-11/12 md:w-4/12 lg:w-3/12 text-teal-500 dark:text-slate-400 shadow-lg transition-colors duration-500">
                 <div className="flex justify-center text-7xl py-3">
                     <FaUserGroup />
                 </div>
                 <div className="text-center">
                     <p className="text-2xl">ჯგუფები</p>
-                    <button className="mt-6 py-1 px-5 rounded border border-teal-500 hover:bg-teal-500 hover:text-white dark:border-slate-400 dark:hover:bg-slate-400 transition-colors duration-300">შექმნა</button>
+                    <button className="mt-6 py-1 px-5 rounded border border-teal-500 hover:bg-teal-500 hover:text-white dark:border-slate-400 dark:hover:bg-slate-400 transition-colors duration-300">დამატება</button>
                 </div>
             </div>
-            <div className="p-4 bg-teal-100/25 dark:bg-slate-500/25 border border-teal-400 dark:border-slate-500 rounded-md w-11/12 md:w-4/12 lg:w-3/12 text-teal-500 dark:text-slate-400 shadow-lg">
+            <div className="p-4 bg-teal-100/25 hover:bg-teal-100 dark:bg-slate-500/25 dark:hover:bg-slate-500/50 border border-teal-400 dark:border-slate-500 rounded-md w-11/12 md:w-4/12 lg:w-3/12 text-teal-500 dark:text-slate-400 shadow-lg transition-colors duration-500">
                 <div className="flex justify-center text-7xl py-3">
                     <RiSurveyFill />
                 </div>
                 <div className="text-center">
                     <p className="text-2xl">გამოკითხვები</p>
-                    <button className="mt-6 py-1 px-5 rounded border border-teal-500 hover:bg-teal-500 hover:text-white dark:border-slate-400 dark:hover:bg-slate-400 transition-colors duration-300">შექმნა</button>
+                    <button className="mt-6 py-1 px-5 rounded border border-teal-500 hover:bg-teal-500 hover:text-white dark:border-slate-400 dark:hover:bg-slate-400 transition-colors duration-300">დამატება</button>
                 </div>
             </div>
         </div>
