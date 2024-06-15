4e1ff08e840360a679bbdaf5ea373a3b440f5e4b
Ioane <ioane.natroshvili08b@geolab.edu.ge>
2024-06-15 13:45:50 +0400

    asdasd

--- src/Components/MainHeader.js
+++ src/Components/MainHeader.js
@@ -6,10 +6,17 @@ import { Link, useLocation } from "react-router-dom";
 const MainHeader = ({ darkMode, changeDarkMode }) => {
     const [currentUrl, setCurrentUrl] = useState("");
     const [navbarCollapse, setNavbarCollapse] = useState(false);
-    const showBurgerMenu = () => setNavbarCollapse(!navbarCollapse);
-
     const [userCollapse, setUserCollapse] = useState(false);
-    const showUserDropdown = () => setUserCollapse(!userCollapse);
+
+    const showBurgerMenu = () => {
+        setNavbarCollapse(!navbarCollapse);
+        setUserCollapse(false);
+    }
+
+    const showUserDropdown = () => {
+        setUserCollapse(!userCollapse);
+        setNavbarCollapse(false);
+    }
 
     const location = useLocation();
 
@@ -25,7 +32,7 @@ const MainHeader = ({ darkMode, changeDarkMode }) => {
                         <a href="#" className="flex items-center ml-2 text-lg">
                             Examinator
                         </a>
-                        <button onClick={showBurgerMenu} data-collapse-toggle="header-navbar" type="button" className="inline-flex items-center ml-auto mr-3 p-2 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="header-navbar" aria-expanded="false">
+                        <button onClick={showBurgerMenu} data-collapse-toggle="header-navbar" type="button" className="z-[11] inline-flex items-center ml-auto mr-3 p-2 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="header-navbar" aria-expanded="false">
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path>
@@ -94,7 +101,7 @@ const MainHeader = ({ darkMode, changeDarkMode }) => {
                                 <img className="w-9 h-9 rounded-full object-cover" src="/img/profile.webp" alt="user photo" />
                             </button>
 
-                            <div id="dropdownAvatar" className={`z-10 ${userCollapse ? '' : 'hidden'} absolute top-[80%] lg:top-[100%] left-0 lg:left-[50%] w-full lg:w-fit lg:translate-x-[-50%] bg-white divide-y divide-gray-100 rounded-lg shadow-md w-44 dark:bg-gray-700 dark:divide-gray-600`}>
+                            <div id="dropdownAvatar" className={`z-10 ${userCollapse ? '' : 'hidden'} absolute top-[80%] lg:top-[100%] left-0 lg:left-[50%] w-full lg:w-fit lg:translate-x-[-50%] bg-white lg:bg-gray-50 divide-y divide-gray-100 rounded-lg shadow-md w-44 dark:bg-gray-700 dark:divide-gray-600`}>
                                 <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
                                     <div>იოანე ნატროშვილი</div>
                                     <div className="font-medium truncate">i.natroshvili@ssu.edu.ge</div>
